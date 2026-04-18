fb5d7202e0223cf6ac0e5f623dc1ac4af8c7f3d3
José Pach <Paching12@icloud.com>
2026-04-18 00:13:13 -0600

    Experimental nodes with style components

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,36 @@ const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
 `;
+const Button = styled.button`
+font-size: 1.4rem;
+padding: 1.2rem 1.6rem;
+font-weight: 500;
+border: none;
+border-radius: 7px;
+background-color: purple;
+color: white;
+margin:  20px;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`
+
+const StyleApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`
 const App = () => {
   return (
-    <div>
+    <StyleApp>
       <H1>The wild OASIS</H1>
-    </div>
+      <Button onClick={() => alert("Check in")}>Check in</Button>
+      <Button onClick={() => alert("Check out")}>Check out</Button>
+
+      <Input type='number' placeholder="Number of guests" /> 
+    </StyleApp>
   );
 };
 
